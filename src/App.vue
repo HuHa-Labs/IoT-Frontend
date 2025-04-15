c10a310e05efa15698001a9985ad214a0380f782
phuongdoan13 <mphuongdoant@gmail.com>
2025-04-15 23:14:47 +1000

    update: sample tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-
+import TheHeader from './components/TheHeader.vue';
 const count = ref(0);
 
 function increment(){
@@ -13,5 +13,8 @@ onMounted(() => {
 </script>
 
 <template>
+  <h1 class="text-5xl underline">
+    Hello world!
+  </h1>
   <button @click="increment">Count is: {{  count  }}</button>
 </template>
