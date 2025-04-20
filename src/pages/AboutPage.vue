4296da7fcf623925b6f4f6c3e2a587b4e4ae49f9
phuongdoan13 <mphuongdoant@gmail.com>
2025-04-20 15:45:34 +1000

    update: add construction message for  AboutPage

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -3,6 +3,6 @@
 
 <template>
   <div class="min-h-screen flex flex-col">
-    About
+    <img src="../assets/underConstruction.png" alt="Vue logo" class="w-32 h-32 mx-auto mt-10" />
   </div>
 </template>
