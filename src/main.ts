a88caf992f764e5361ee54b3d509e6d52042ff5f
phuongdoan13 <mphuongdoant@gmail.com>
2025-04-21 22:30:39 +1000

    refactor: rename Router.ts to router.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { router } from './plugins/Router'
+import { router } from './plugins/router.ts'
 
 createApp(App)
 .use(router)
