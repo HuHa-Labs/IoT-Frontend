93707987dc91835e4ce02eacb5968aac50337bd0
phuongdoan13 <mphuongdoant@gmail.com>
2025-04-27 21:21:38 +1000

    format: remove gaps

--- src/components/charts/LastRecordChart.vue
+++ src/components/charts/LastRecordChart.vue
@@ -4,26 +4,24 @@ const latestRecordStore = useLatestRecordStore();
 </script>
 
 <template>
-  <div class="grid grid-cols-[auto_1fr] gap-x-4 gap-y-2 text-base font-medium">
+  <div
+    class="grid grid-cols-[auto_1fr] gap-x-4 gap-y-2 text-base font-medium">
     <div>🌡 Temperature️:</div>
     <div>
       {{ latestRecordStore.temp }}°C
     </div>
-
     <div>💦
       Humidity:
     </div>
     <div>
       {{ latestRecordStore.humidity }}%
     </div>
-
     <div>
       🔌 Electricity usage:
     </div>
     <div>
       {{ latestRecordStore.electricityUsage }}W
     </div>
-
     <div>
       🐾 Motion detected:
     </div>
