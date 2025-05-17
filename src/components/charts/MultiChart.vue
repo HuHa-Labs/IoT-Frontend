0d15936fc89ab3af8409db3a22c832403ddfea22
phuongdoan13 <mphuongdoant@gmail.com>
2025-05-17 22:32:14 +1000

    update: multi-chart data store

---
+++ src/components/charts/MultiChart.vue
@@ -0,0 +1,9 @@
+<template>
+</template>
+
+<script setup lang="ts">
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend } from 'chart.js';
+
+
+</script>
---
+++ src/stores/multi-data-store.ts
@@ -0,0 +1,74 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useMultiDataStore = defineStore("multiData", () => {
+  const config = ref({
+    type: 'line',
+    data: data,
+    options: {
+      responsive: true,
+      interaction: {
+        mode: 'index',
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: true,
+          text: 'Chart.js Line Chart - Multi Axis'
+        }
+      },
+      scales: {
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+      }
+    },
+  });
+
+  const DATA_COUNT = ref(7);
+  const NUMBER_CFG = ref({
+    count: DATA_COUNT,
+    min: -100,
+    max: 100
+  });
+
+  const labels = Utils.getMonths({count: 7});
+  const data = ref{
+    labels: labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: Utils.numbers(NUMBER_CFG),
+        borderColor: Utils.CHART_COLORS.red,
+        backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+        yAxisID: 'y',
+      },
+      {
+        label: 'Dataset 2',
+        data: Utils.numbers(NUMBER_CFG),
+        borderColor: Utils.CHART_COLORS.blue,
+        backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+        yAxisID: 'y1',
+      }
+    ]
+  });
+  return {
+    temp,
+    humidity,
+    electricityUsage,
+    motionDetected,
+  };
+});
